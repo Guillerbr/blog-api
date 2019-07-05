d751b62fce95c97fbf772d19d160abaca6c45dad
Guillerbr <ghminvest@gmail.com>
2019-07-04 21:22:57 -0300

    add db connect and middlewares

--- app.js
+++ app.js
@@ -6,3 +6,30 @@ const cors = require('cors');
 
 const app = express();
 
+const db = require('./config/db').database;
+
+// database connection
+
+mongoose.connect(db,
+     {useNewUrlParser: true})
+  .then(() => {
+
+    console.log('Database Connected Sucess')
+
+  })
+  .catch((err) => {
+      console.log('Unable to connect with the database', err)
+  });
+
+
+  //Define the PORT
+  const port = process.env.PORT || 5000;
+
+  // initialize cors middleware
+
+  app.use(cors());
+
+  // initialize BodyParser Middleware
+  app.use(bodyParser.json());
+
+  app.get('/', (req,res))
--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     Database : 'mongodb://localhost:27017/blog-api-app',
-    
-}
+    secret: '12345'
+};
