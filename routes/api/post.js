4cc74127c9bd9c4bad8d63264cd4721c2961197c
Guillerbr <ghminvest@gmail.com>
2019-07-05 01:36:34 -0300

    get,post,put api add

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.router();
 const Post = require('../../models/Post');
 
+//get all the posts
 router.get('/', (req, res, next) => {
     Post.find()
         .then((posts) => {
@@ -9,6 +10,47 @@ router.get('/', (req, res, next) => {
 
         })
         .catch(err => console.log(err))
+});
+
+//create a post
+router.post('/add', (req, res, next) => {
+    const title = req.body.title;
+    const body = req.body.body;
+
+
+    newPost = new Post({
+        title: title,
+        body: body
+    });
+    newPost.save()
+        .then(post => {
+            res.json(post);
+        })
+        .catch(err => console.log(err));
+})
+
+// to update a post
+router.put('/update/:id', (req, res, next) => {
+    //grab the id of the post
+    let id = req.params.id;
+
+    //find the post byID from the database
+    Post.findById(id)
+        .then(post => {
+            post.title = req.body.title;
+            post.body = req.body.body;
+            post.save()
+                .then(post => {
+                    res.send({
+                        message: 'Post udated successfuly',
+                        status: 'success',
+                        post: post
+                    })
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+
 });
 
 module.exports = router;
